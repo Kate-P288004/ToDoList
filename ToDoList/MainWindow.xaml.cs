8ea80d35b59abc79bc2f19e1046aac8812d67422
p288004 <p288004@MD-T103-611611>
2025-07-24 10:57:22 +0800

    build

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,41 +1,25 @@
-﻿using System.Diagnostics;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ToDoList
+﻿using System.Windows;
+namespace CuteToDoApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
         private void AddHomeTask_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(HomeInput.Text))
             {
-                HomeList.Items.Add( HomeInput.Text);
+                HomeList.Items.Add(HomeInput.Text);
                 HomeInput.Clear();
             }
         }
-
         private void AddTafeTask_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(TafeInput.Text))
             {
-                TafeList.Items.Add( TafeInput.Text);
+                TafeList.Items.Add(TafeInput.Text);
                 TafeInput.Clear();
             }
         }
@@ -43,10 +27,9 @@ namespace ToDoList
         {
             if (!string.IsNullOrWhiteSpace(HobbyInput.Text))
             {
-                HobbyList.Items.Add( HobbyInput.Text);
+                HobbyList.Items.Add(HobbyInput.Text);
                 HobbyInput.Clear();
             }
         }
-
     }
 }
